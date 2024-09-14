2e108cfb2a06a1c5bf08649c830643afbe725f64
Diana111U <diana_ustinova11@mail.ru>
2024-09-14 17:34:00 +0300

    some second

---
+++ untitled/src/task3.1.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+
+    val arrAbcNum =mapOf( 21 to "А", 13 to "Б",4 to "В",20 to "Г",22 to "Д",1 to "Е",25 to "Ё",12 to "Ж",24 to "З",14 to "И",2 to "Й",28 to "К",9 to "Л",23 to "М",3 to "Н",29 to "О",6 to "П",16 to "Р",15 to "С",11 to "Т",26 to "У",5 to "Ф",30 to "Х",27 to "Ц",8 to "Ч",18 to "Ш",10 to "Щ",33 to "Ь",31 to "Ы",32 to "Ъ",19 to "Э",7 to "Ю",17 to "Я")
+    print("Введите ключевое слово: ")
+    val keyWord = readln()
+    print("Исходный текст: ")
+    val basedText = readln()
+
+}
